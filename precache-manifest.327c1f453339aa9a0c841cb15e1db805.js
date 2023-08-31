773302403ca4718b6593f5e986fe7febaeb76e45
Yurii <Sunshineju0213@gmail.com>
2023-08-31 18:22:53 +0900

    Updates

---
+++ precache-manifest.327c1f453339aa9a0c841cb15e1db805.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b18b774262e07bc33005481d5381e0d8",
+    "url": "/sunju-portfolio/index.html"
+  },
+  {
+    "revision": "9696469700b21d4da35a",
+    "url": "/sunju-portfolio/static/css/main.10fe0f47.chunk.css"
+  },
+  {
+    "revision": "459adfa0c3bcf0f99eae",
+    "url": "/sunju-portfolio/static/js/2.fbde5573.chunk.js"
+  },
+  {
+    "revision": "37fdc2d2e258a4a0ff71fb7bf84cf091",
+    "url": "/sunju-portfolio/static/js/2.fbde5573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9696469700b21d4da35a",
+    "url": "/sunju-portfolio/static/js/main.32afe818.chunk.js"
+  },
+  {
+    "revision": "038be9abc2a426bdf50d",
+    "url": "/sunju-portfolio/static/js/runtime-main.8e520900.js"
+  },
+  {
+    "revision": "9e14855ef75fd749beabd9ca2c1a7577",
+    "url": "/sunju-portfolio/static/media/discordIcon.9e14855e.svg"
+  },
+  {
+    "revision": "418b0aa275bed12b084b274929ed2ae6",
+    "url": "/sunju-portfolio/static/media/header__bg.418b0aa2.png"
+  },
+  {
+    "revision": "3282563b313207a990fd684a9f0599e7",
+    "url": "/sunju-portfolio/static/media/hero-image.3282563b.png"
+  },
+  {
+    "revision": "b47e51889c63ce65354ecb3e3fff531a",
+    "url": "/sunju-portfolio/static/media/logo_main1.b47e5188.png"
+  },
+  {
+    "revision": "637736a0e412663659b77a029b5cf03a",
+    "url": "/sunju-portfolio/static/media/skypeIcon.637736a0.png"
+  }
+]);
